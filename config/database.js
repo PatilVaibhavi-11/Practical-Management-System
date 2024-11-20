2785ba8ceb0b03c2723562a05f11e65db09055be
PatilVaibhavi-11 <vaibhavipa02@gmail.com>
2024-11-20 23:25:59 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 mongoose.set("strictPopulate", false);
 const dbConnect=async()=>{
     try {
-       await mongoose.connect("mongodb+srv://vaibhavipa02:Vaibhavi2011@practicalcluster.rdlzm.mongodb.net/PracticalManagement")
+       await mongoose.connect(process.env.MONGO_DB_URL)
        console.log("MongoDB connected successfully")
     } catch (error) {
         console.log("Connection failed",error)
